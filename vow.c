4fae0bad87071205a41d38ce1cff9a6683031c77
dhivya284 <41949839+dhivya284@users.noreply.github.com>
2018-07-31 10:53:29 +0000

    Add files via upload

---
+++ vow.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	char ch;
+	scanf("%d",&ch);
+	if(ch='a','e','i','o','u')
+	{
+		printf("Vowel");
+	}
+	else if(ch!='a','e','i','o','u');
+	{
+		printf("Consonant");
+	}
+	else
+	{
+		printf("invalid");
+	}
+}
+
+
